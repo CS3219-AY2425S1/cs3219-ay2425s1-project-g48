ab844473934c1ef34b8100654fd6c73d58cd763c
davidni <davidni055@gmail.com>
2024-09-20 22:42:10 +0800

    Modify authenfication in backend

--- backend/main.go
+++ backend/main.go
@@ -22,12 +22,12 @@ func main() {
 
     router.Use(middleware.Authentication())
 
+    // API-2
+    router.GET("/api-1", func(c *gin.Context) {
 
-	// defer func() {
-	// 	if err := database.Client.Disconnect(context.TODO()); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
+        c.JSON(200, gin.H{"success": "Access granted for api-1"})
+
+    })
 
     // API-1
     router.GET("/api-2", func(c *gin.Context) {
