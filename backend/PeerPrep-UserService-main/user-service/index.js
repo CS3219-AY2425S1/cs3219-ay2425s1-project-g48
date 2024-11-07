a95ffaa76bffcf19637aeffced45271fe1a4b3ca
Aaron <taykaiboon12@gmail.com>
2024-11-07 11:30:54 +0800

    Testing the api gateway

--- backend/PeerPrep-UserService-main/user-service/index.js
+++ backend/PeerPrep-UserService-main/user-service/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
 
   // Browsers usually send this before PUT or POST Requests
@@ -30,10 +30,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/users", userRoutes);
-app.use("/auth", authRoutes);
+app.use("/user/users", userRoutes);
+app.use("/user/auth", authRoutes);
 
-app.get("/", (req, res, next) => {
+app.get("/user", (req, res, next) => {
   console.log("Sending Greetings!");
   res.json({
     message: "Hello World from user-service",
