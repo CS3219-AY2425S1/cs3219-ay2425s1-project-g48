934d114b9863595264c38833c6b4a3dd9d637863
Aaron <taykaiboon12@gmail.com>
2024-11-08 13:13:50 +0800

    question service ingress

--- backend/QuestionService/routes/questionRoutes.go
+++ backend/QuestionService/routes/questionRoutes.go
@@ -8,12 +8,22 @@ import (
 
 // UserRoutes function
 func QuestionRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.GET("/questions", controller.GetQuestions)
-	incomingRoutes.GET("/questions/topics", controller.GetTopics)
-	incomingRoutes.GET("/questionsById", controller.GetQuestionsById)
-	incomingRoutes.PUT("/questions", controller.UpdateQuestion)
-	incomingRoutes.DELETE("/questions", controller.DeleteQuestion)
-	incomingRoutes.POST("/questions", controller.AddQuestionToDb())
-	incomingRoutes.POST("/questions/leetcode", controller.AddLeetCodeQuestionToDb())
-	incomingRoutes.POST("/questions/matching", controller.AssignQuestion)
+	routerGroup := incomingRoutes.Group("/question")
+	routerGroup.GET("/questions", controller.GetQuestions)
+	routerGroup.GET("/questions/topics", controller.GetTopics)
+	routerGroup.GET("/questionsById", controller.GetQuestionsById)
+	routerGroup.PUT("/questions", controller.UpdateQuestion)
+	routerGroup.DELETE("/questions", controller.DeleteQuestion)
+	routerGroup.POST("/questions", controller.AddQuestionToDb())
+	routerGroup.POST("/questions/leetcode", controller.AddLeetCodeQuestionToDb())
+	routerGroup.POST("/questions/matching", controller.AssignQuestion)
+
+	// incomingRoutes.GET("/questions", controller.GetQuestions)
+	// incomingRoutes.GET("/questions/topics", controller.GetTopics)
+	// incomingRoutes.GET("/questionsById", controller.GetQuestionsById)
+	// incomingRoutes.PUT("/questions", controller.UpdateQuestion)
+	// incomingRoutes.DELETE("/questions", controller.DeleteQuestion)
+	// incomingRoutes.POST("/questions", controller.AddQuestionToDb())
+	// incomingRoutes.POST("/questions/leetcode", controller.AddLeetCodeQuestionToDb())
+	// incomingRoutes.POST("/questions/matching", controller.AssignQuestion)
 }
